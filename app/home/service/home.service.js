e2324b8f5fc71aee742fc96dfa9b195e14ab1789
Debasisha Bhanja <Debasisha-Bhanja@users.noreply.github.com>
2016-06-09 06:50:32 -0500

    Update home.service.js

--- app/home/service/home.service.js
+++ app/home/service/home.service.js
@@ -1,1 +1,18 @@
+(function () {
+  'use strict';
+
+  /* @ngInject */
+  angular
+    .module('home.services', [])
+    .factory('PostData', Post)
+
+  /* @ngInject */
+  function Post ($resource, BaseUrl) {
+	 console.info('Service ....');
+    return "Post Data";
+  }
+
+ 
+
+})();
 
